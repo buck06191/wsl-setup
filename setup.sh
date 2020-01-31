acc62298917b16d34e3bd12f28046ece67414a32
Josh Buckland <buck06191@gmail.com>
2020-01-31 15:38:16 +0000

    Fix bug with zshrc deletion. Add status updates during install.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 # install zsh and oh-my-zsh
+echo "1/5 Installing zsh and oh-my-zsh\n"
 sudo apt-get -y install zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Get theme
+echo "2/5 Installing powerlevel10k\n"
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 ( cd && curl -fsSLO https://raw.githubusercontent.com/romkatv/dotfiles-public/master/.purepower )
 
 # Install plugins
+echo "3/5 Installing plugins\n"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # move the repo .zshrc into HOME
-mv ./.zshrc ~/.zshrc
+echo "4/5 Copying over zshrc file"
+cp ./.zshrc ~/.zshrc
 
 # Apparently this speeds up pasting into the terminal...
+echo "5/5 Speeding up pasting into terminal"
 mkdir -p $ZSH_CUSTOM/lib && touch $ZSH_CUSTOM/lib/misc.zsh
 
 source ~/.zshrc
